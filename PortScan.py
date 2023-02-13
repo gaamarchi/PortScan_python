0ebca54e6c325882ddf7f15ac03fa07df983d428
gabriel marchi <g.marchi.madureira@gmail.com>
2023-02-13 13:10:01 -0300

    adicionando modo silencioso

--- PortScan.py
+++ PortScan.py
@@ -20,19 +20,29 @@ def main():
     parser.add_argument('host', help='target host')
     parser.add_argument('-p','--ports',nargs="+",type=int, help='ports to scan')
     parser.add_argument('-t','--timeout',type=int,default=5,help='connetcct timeout in seconds')
+    parser.add_argument('-v','--verbose',help='verbose mode',default= False,type=bool)
     args = parser.parse_args()
 
     host = args.host
     ports = args.ports or range(1,65535)
     timeout = args.timeout
-
+    verbose = args.verbose
+    print(verbose)
     print("Scanning Host",host,"...")
     start_time = datetime.now()
     print("start time ", start_time)
-    for port in ports:
-        if port_scan(host,port,timeout):
+    if verbose:
+        
+        for port in ports:
+            if port_scan(host,port,timeout):
+                print("port",port,"is open")
+    else:
+        Open_ports = []
+        for port in ports:
+            if port_scan(host,port,timeout):
+                Open_ports.append(port)
+        for port in Open_ports:
             print("port",port,"is open")
-    
 
     print("Scanning finished in",datetime.now()-start_time)
 
